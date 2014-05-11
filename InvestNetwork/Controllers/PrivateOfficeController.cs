d6a8d79021a6b541e02bf2594f106ca28a416149
SpiriT <x-spirit-alex@mail.ru>
2014-05-11 16:33:48 +0400

    Личный кабинет

---
+++ InvestNetwork/Controllers/PrivateOfficeController.cs
@@ -0,0 +1,36 @@
+﻿using InvestNetwork.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InvestNetwork.Controllers
+{
+    public class PrivateOfficeController : Controller
+    {
+        //
+        // GET: /UsersInfo/
+        private readonly IUsersInfoRepository _usersInfoRepository;
+        private readonly IInvestContext _investContext;
+
+        public PrivateOfficeController(IUsersInfoRepository usersInfoRepository, IInvestContext investContext)
+        {
+            _usersInfoRepository = usersInfoRepository;
+            _investContext = investContext;
+        }
+
+        [HttpGet]
+        [Authorize]
+        public ActionResult Index()
+        {
+            User user = _investContext.CurrentUser;
+            user.Avatar = "http://placehold.it/210x230";
+
+            ViewBag.usersInfo = _usersInfoRepository.GetByUserId(user.Id);
+            ViewBag.partycipations = _usersInfoRepository.GetPartycipation(user.ID);
+
+            return View(user);
+        }
+    }
+}
