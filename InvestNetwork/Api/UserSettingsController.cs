091b1b819cb0e2ceaf7646ebe8eb2983ea819881
SpiriT <x-spirit-alex@mail.ru>
2014-05-14 02:13:14 +0400

    Недоделанная отправка сообщений

---
+++ InvestNetwork/Api/UserSettingsController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Web.Http;
+using InvestNetwork.Models;
+
+namespace InvestNetwork.Api
+{
+    public class UserSettingsController : ApiController
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IInvestContext _investContext;
+
+        public UserSettingsController(IUserRepository userRepository, IInvestContext investContext)
+        {
+            _userRepository = userRepository;
+            _investContext = investContext;
+        }
+
+        [Authorize]
+        [HttpPost]
+        public object Edit(UserSettings model)
+        {
+            User user = _investContext.CurrentUser;
+
+            if (model.NewPassword != null)
+            {
+
+                if (!model.OldPassword.Equals(user.Password))
+                {
+                    return new { isSuccess = false, errorMessage = "Действующий пароль введен неверно", successMessage = "" };
+                }
+
+                if (!model.NewPassword.Equals(model.ConfirmPassword))
+                {
+                    return new { isSuccess = false, errorMessage = "Подтвердите новый пароль", successMessage = "" };
+                }
+
+                user.Password = model.NewPassword.ToString();
+            }
+
+            if (model.Email == null)
+            {
+                model.Email = user.Email;
+            }
+            else
+            {
+                if (!model.Email.Equals(user.Email))
+                {
+                    var anyUser = _userRepository.GetAll().Any(p => p.Email.Equals(model.Email));
+                    if (anyUser)
+                    {
+                        return new { isSuccess = false, errorMessage = "Пользователь с таким email уже зарегистрирован", successMessage = "" };
+                    }
+
+                    Regex rgx = new Regex("^[a-z0-9_\\+-]+(\\.[a-z0-9_\\+-]+)*@[a-z0-9-]+(\\.[a-z0-9-]+)*\\.([a-z]{2,4})$");
+                    if (!rgx.IsMatch(model.Email))
+                    {
+                        return new { isSuccess = false, errorMessage = "Введен некорректный email", successMessage = "" };
+                    }
+                    user.Email = model.Email;
+                }
+            }
+
+            if (model.FullName == null)
+            {
+                model.FullName = user.FullName;
+            }
+            else
+            {
+                if (!model.FullName.Equals(user.FullName))
+                {
+                    var anyUser = _userRepository.GetAll().Any(p => p.FullName.Equals(model.FullName));
+                    if (anyUser)
+                    {
+                        return new { isSuccess = false, errorMessage = "Пользователь с таким именем уже зарегистрирован", successMessage = "" };
+                    }
+                }
+                user.FullName = model.FullName;
+            }
+
+            if (model.Avatar == null)
+            {
+                model.Avatar = user.Avatar;
+            }
+
+            _userRepository.Update(user);
+
+            _userRepository.SaveChanges();
+
+            return new { isSuccess = true, errorMessage = "", successMessage = "Данные успешно сохранены" };
+        }
+    }
+}
---
+++ InvestNetwork/Models/Repository/Implementations/MessageRepository.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Entity;
+using System.Threading.Tasks;
+using InvestNetwork.Core;
+using System.Linq.Expressions;
+
+namespace InvestNetwork.Models
+{
+    public class MessageRepository : IMessageRepository
+    {
+        private IRepository<Message> messageRepository;
+        private IRepository<User> userRepository;
+
+        public MessageRepository(IRepository<Message> messageRepository, IRepository<User> userRepository)
+        {
+            this.messageRepository = messageRepository;
+            this.userRepository = userRepository;
+        }
+        public List<Message> GetAll()
+        {
+            return messageRepository.GetAll().ToList();
+        }
+
+        public Message GetById(int id)
+        {
+            if (id == 0)
+                return null;
+            return messageRepository.GetById(id);
+        }
+
+        public List<Message> GetByUserId(int id)
+        {
+            if (id == 0)
+                return null;
+            var list = messageRepository.GetAll().Where(e => e.ToUserID == id).OrderBy(e => e.MessageDate).ToList();
+            foreach(Message message in list)
+            {
+                message.User = userRepository.GetById(message.FromUserID);
+            }
+            return list;
+        }
+
+        public void Insert(Message model)
+        {
+            if (model == null)
+                throw new ArgumentNullException("message");
+            messageRepository.Insert(model);
+        }
+
+        public void Update(Message model)
+        {
+            if (model == null)
+                throw new ArgumentNullException("message");
+            messageRepository.Update(model);
+
+        }
+
+        public void Delete(Message model)
+        {
+            if (model == null)
+                throw new ArgumentNullException("message");
+            messageRepository.Delete(model);
+        }
+
+        public void SaveChanges()
+        {
+            messageRepository.SaveChanges();
+        }
+    }
+}
+
